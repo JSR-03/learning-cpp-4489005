935dbba76a43da273f05c6ee5f6556d3f554e519
JSR-03 <sheltonrhodes03@gmail.com>
2025-09-02 20:52:01 +0000

    Finished 03_06

--- src/Ch03/03_05b/CodeDemo.cpp
+++ src/Ch03/03_05b/CodeDemo.cpp
@@ -6,33 +6,6 @@
 #include <string>
 #include "cow.h"
 
-enum class cow_purpose {dairy, meat, hide, pet};
-
-class cow{
-public:
-    cow(std::string name_i, int age_i, cow_purpose purpose_i){
-        name = name_i;
-        age = age_i;
-        purpose = purpose_i;
-    }
-    std::string get_name() const{
-        return name;
-    }
-    int get_age() const{
-        return age;
-    }
-    cow_purpose get_purpose() const{
-        return purpose;
-    }
-    void set_age(int new_age){
-        age = new_age;
-    }
-private:
-    std::string name;
-    int age;
-    cow_purpose purpose;
-};
-
 int main(){
     cow my_cow("Hildy", 7, cow_purpose::pet);
     std::cout << my_cow.get_name() << " is a type-" << (int) my_cow.get_purpose() << " cow." << std::endl;
